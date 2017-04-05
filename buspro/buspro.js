da88eeec0efad236f857ac68e81fe0f6764db695
efa2000 <efa@e-dev>
2017-04-05 20:30:07 +0300

    add bus support

--- buspro/buspro.js
+++ buspro/buspro.js
@@ -5,22 +5,38 @@ module.exports = function(RED) {
     function BusproControllerNode(n) {
         RED.nodes.createNode(this,n);
         this.host = n.host;
-        this.port = n.port;
-        this.subnetid = n.subnetid;
-        this.deviceid = n.deviceid;
+        this.port = n.port || 6000;
+        this.deviceid = parseInt(n.subnetid)+"."+parseInt(n.deviceid);
+        var node = this;
+		this.bus = new SmartBus({
+  			device: node.deviceid,      // Connector address in HDL network (subnet.id)
+  			gateway: node.host, 		// HDL SmartBus gateway IP
+  			port: node.port                	// and port, default: 6000
+		});
+		this.on("close",function(){
+			node.bus.removeAllListeners();
+			node.bus.socket.close();
+		})        
     }
     RED.nodes.registerType("buspro-controller",BusproControllerNode);
 
 
-    function LowerCaseNode(config) {
+    function BusproIn(config) {
         RED.nodes.createNode(this,config);
+        this.bus = RED.nodes.getNode(config.controller);
         var node = this;
-        this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
+		this.bus.on('command', function(command) {
+		  // Integer with command operation code
+		  //command.code;
+		  // Device objects
+		  //command.sender;
+		  //command.target;
+		  // Object with decoded data or raw buffer
+		  // if data can not be parsed automatically
+		  //command.data;
+		});        
     }
-    RED.nodes.registerType("lower-case",LowerCaseNode);
+    RED.nodes.registerType("buspro-in",BusproIn);
 
 
 }
